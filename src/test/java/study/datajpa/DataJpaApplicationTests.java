9c2bde41f6fae3148054974ea87de03f506aa6c5
sayhara <impact82@naver.com>
2021-10-21 17:21:38 +0900

    make project

---
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package study.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// File -> Settings -> gradle
+// gradle -> IntelliJ IDEA로 변경
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
